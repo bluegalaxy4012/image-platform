62244f52f6b72db92814bfaa03fa3ce0835f47e2
Marian Dumitriu <bluegalaxy40@yahoo.com>
2025-08-14 15:40:51 +0300

    removed nginx, self-certs. using caddy (with tls) on ec2

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,12 @@
+// import useState from "react";
+// import apiClient from "../api/ApiClient";
+
+export default function Register() {
+
+
+    return (
+        <div>
+           <p> Register </p> 
+        </div>
+    );
+}
